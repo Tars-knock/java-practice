3777ff61e0d4b4d34026a88440708be3a15226e9
Tars-knock <sfly13344@163.com>
2021-01-03 21:43:19 +0800

    完善功能

--- calendar/src/main.java
+++ calendar/src/main.java
@@ -1,3 +1,4 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 //使用LocalDate类  绘制当月日历
 public class main {
@@ -6,6 +7,25 @@ public class main {
         LocalDate now = LocalDate.now();
         int month = now.getMonthValue();
         int today = now.getDayOfMonth();
-                
+        LocalDate start = now.minusDays(today - 1);//获取本月一号对象
+
+        DayOfWeek weekday = start.getDayOfWeek();//查看本月一号是周几
+        int value = weekday.getValue();// 1 = Mon ; 7 = Sun
+
+        System.out.println("Mon  Tue  Wed  Thu  Fri  Sat  Sun");
+        for(int i =1; i < value ;i++)
+        {
+            System.out.print("     ");//一号缩进
+        }
+        while(start.getMonthValue() == month)
+        {
+            System.out.printf("%-3d",start.getDayOfMonth());
+            if (start.getDayOfMonth()==today)
+                System.out.print("* ");   //当日标记
+            else System.out.print("  ");
+
+            if(start.getDayOfWeek().getValue()==7) System.out.println();//周末回车
+            start = start.plusDays(1);
+        }
     }
 }
