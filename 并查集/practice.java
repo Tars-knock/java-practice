9e7631b27952a011c75e373bd064ab00a1eacffc
Tars-knock <sfly13344@163.com>
2021-01-18 21:48:42 +0800

    使用面向对象方法构建并查集类

--- 并查集/practice.java
+++ 并查集/practice.java
@@ -39,3 +39,25 @@ class Solution {
 }
 //题目详见 https://leetcode-cn.com/problems/redundant-connection/
 //并查集定义详见 https://oi-wiki.org/ds/dsu/
+
+class UnionFind {
+    int[] parent;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public void union(int index1, int index2) {
+        parent[find(index2)] = find(index1);
+    }
+
+    public int find(int index) {
+        if (parent[index] != index) {
+            parent[index] = find(parent[index]);
+        }
+        return parent[index];
+    }
+}
