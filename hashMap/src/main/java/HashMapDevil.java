c4b63b5543cb75dbb483c6b174c76044090798e2
Tars-knock <sfly13344@163.com>
2022-03-25 23:59:17 +0800

    十万线程并发写

---
+++ hashMap/src/main/java/HashMapDevil.java
@@ -0,0 +1,20 @@
+import java.net.Socket;
+import java.util.HashMap;
+
+public class HashMapDevil {
+    private static HashMap<Long, Socket> hashMap = new HashMap<Long,Socket>();
+
+    public static void main(String[] args) {
+
+        for (int i = 0 ; i < 100000 ; i++){
+            (new Thread(){
+                @Override
+                public void run() {
+                    hashMap.put(System.nanoTime(),new Socket());
+                }
+            }).start();
+            System.out.println("========创建第"+i+"个线程");
+        }
+    }
+}
+
