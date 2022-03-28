2c23a851241fe5d62cca2f499a2f52187a84e07d
Tars-knock <sfly13344@163.com>
2022-03-28 23:39:27 +0800

    线程池练习

---
+++ multi-thread/src/main/java/ExecutorTest.java
@@ -0,0 +1,6 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorTest {
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
+}
